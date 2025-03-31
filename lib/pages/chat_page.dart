3b7d8a76fae396b1de0c6a89771c7c2d945838ff
Damin Rido <daminrido139@gmail.com>
2025-03-31 16:08:49 +0530

    UI improved

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,4 +1,6 @@
+import 'package:agl_quiz/providers/theme_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import '../services/gemini_service.dart';
 
 class ChatScreen extends StatefulWidget {
@@ -34,70 +36,88 @@ class _ChatScreenState extends State<ChatScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("AI Chat", style: TextStyle(fontSize: 24),),
+        title: Text(
+          "AI Chat",
+          style: TextStyle(fontSize: 24),
+        ),
         toolbarHeight: 100,
-
         backgroundColor: Theme.of(context).scaffoldBackgroundColor,
         scrolledUnderElevation: 0,
       ),
       body: Column(
         children: [
           Expanded(
-            child: ListView.builder(
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              itemCount: _messages.length,
-              itemBuilder: (context, index) {
-                final msg = _messages[index];
-                bool isUser = msg["role"] == "user";
-
-                return Align(
-                  alignment:
-                      isUser ? Alignment.centerRight : Alignment.centerLeft,
-                  child: ConstrainedBox(
-                    constraints: BoxConstraints(maxWidth: 1000),
-                    child: Container(
-                      
-                      margin: EdgeInsets.symmetric(vertical: 4, horizontal: 10),
-                      padding: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        color: isUser ? Colors.green.shade700 : Colors.grey.shade300,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Text(
-                        msg["text"]!,
-                        style: TextStyle(
-                            color: isUser ? Colors.white : Colors.black),
-                      ),
+            child: _messages.isEmpty
+                ? Center(
+                    child: Text(
+                      "An example of integrating an API...",
+                      style: TextStyle(fontSize: 48),
                     ),
+                  )
+                : ListView.builder(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                    itemCount: _messages.length,
+                    itemBuilder: (context, index) {
+                      final msg = _messages[index];
+                      bool isUser = msg["role"] == "user";
+
+                      return Align(
+                        alignment: isUser
+                            ? Alignment.centerRight
+                            : Alignment.centerLeft,
+                        child: ConstrainedBox(
+                          constraints: BoxConstraints(maxWidth: 1000),
+                          child: Container(
+                            margin: EdgeInsets.symmetric(
+                                vertical: 4, horizontal: 10),
+                            padding: EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                              color: isUser
+                                  ? Colors.green.shade700
+                                  : Colors.grey.shade300,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Text(
+                              msg["text"]!,
+                              style: TextStyle(
+                                  color: isUser ? Colors.white : Colors.black),
+                            ),
+                          ),
+                        ),
+                      );
+                    },
                   ),
-                );
-              },
-            ),
           ),
           Padding(
-            padding: EdgeInsets.all(8.0),
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
             child: Row(
               children: [
                 SizedBox(width: 20),
                 Expanded(
                   child: TextField(
-                    controller: _controller,
-                    decoration: InputDecoration(
-                      hintText: "Type a message...",
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.green)
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.green)
-                      ),
-                    ),
-                  ),
+                      controller: _controller,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor:
+                            Provider.of<ThemeProvider>(context).isDarkMode
+                                ? Colors.grey.shade800
+                                : const Color.fromARGB(255, 209, 209, 209),
+                        hintText: "Type a message...",
+                        border: OutlineInputBorder(borderSide: BorderSide.none),
+                        enabledBorder:
+                            OutlineInputBorder(borderSide: BorderSide.none),
+                        focusedBorder:
+                            OutlineInputBorder(borderSide: BorderSide.none),
+                      )),
                 ),
                 SizedBox(width: 20),
-
-                IconButton(
-                  icon: Icon(Icons.send, color: Colors.green),
-                  onPressed: _sendMessage,
+                Container(
+                  decoration: ShapeDecoration(
+                      shape: CircleBorder(), color: Colors.green),
+                  child: IconButton(
+                    icon: Icon(Icons.send),
+                    onPressed: _sendMessage,
+                  ),
                 ),
               ],
             ),
